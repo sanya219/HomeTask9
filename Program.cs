1cd14aaf09c494a0247e2acc829358809d7b59b7
Alexander Daurskikh <sanya219@gmail.com>
2023-02-26 06:53:54 +0100

    Task 68 solved

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 Выполнить с помощью рекурсии.
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
-/*
+
 void AllNaturals (int max)
 {
     if (max > 1) 
@@ -16,7 +16,7 @@ void AllNaturals (int max)
 }
 
 Console.Write("Enter number N: ");
-AllNaturals(Convert.ToInt32(Console.ReadLine())); */
+AllNaturals(Convert.ToInt32(Console.ReadLine())); 
 
 /* Задача 66: Задайте значения M и N. 
 Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
@@ -34,3 +34,22 @@ int numberM = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter first number (N): ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"The sum of natural numbers from {numberM} to {numberN} is {SumOfElements(numberM,numberN)}");
+
+/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 */
+
+int AkkermanFunction(int numberM, int numberN)
+{
+    if(numberM == 0)
+        return numberN + 1;
+    else if(numberN == 0)
+        return AkkermanFunction(numberM - 1, 1);
+    else 
+        return AkkermanFunction(numberM - 1, AkkermanFunction(numberM, numberN - 1)); 
+}
+Console.Write("Enter first m parameter: ");
+numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter first n parameter: ");
+numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Akkerman function f({numberM}, {numberN} = {AkkermanFunction(numberM, numberN)}");
