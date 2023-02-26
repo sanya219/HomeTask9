0f93628a1d695f37895a302214941a906d74357c
Alexander Daurskikh <sanya219@gmail.com>
2023-02-26 06:45:44 +0100

    Task 66 solved

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 Выполнить с помощью рекурсии.
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
-
+/*
 void AllNaturals (int max)
 {
     if (max > 1) 
@@ -16,4 +16,21 @@ void AllNaturals (int max)
 }
 
 Console.Write("Enter number N: ");
-AllNaturals(Convert.ToInt32(Console.ReadLine()));
+AllNaturals(Convert.ToInt32(Console.ReadLine())); */
+
+/* Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int SumOfElements(int first, int last)
+{
+    if(first == last)
+        return first;
+    return first + SumOfElements(first + 1, last);
+}
+Console.Write("Enter first number (M): ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter first number (N): ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The sum of natural numbers from {numberM} to {numberN} is {SumOfElements(numberM,numberN)}");
